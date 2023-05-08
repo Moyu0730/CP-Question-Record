c255d4a43e938e50d568ee32dcb19169fd81a3fc
Moyu0730 <aslanchen0730@gmail.com>
2023-05-08 20:36:33 +0800

    【2023. 05. 08】First Commit

---
+++ Set.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<math.h>
+using namespace std;
+
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+
+signed main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cout << M_PI;
+}
