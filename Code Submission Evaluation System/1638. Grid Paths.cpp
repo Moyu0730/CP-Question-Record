d472ce4e769ab143abb71ac288ef99567fdacf7b
Moyu0730 <aslanchen0730@gmail.com>
2023-05-10 09:34:37 +0800

    【2023. 05. 10】1638. Grid Paths - Solved

--- Code Submission Evaluation System/1638. Grid Paths.cpp
+++ Code Submission Evaluation System/1638. Grid Paths.cpp
@@ -15,22 +15,7 @@ const int Mod = 1e9 + 7;
 char grid[MAXN][MAXN];
 int n, dp[MAXN][MAXN];
 
-int sol( int i, int j ){
-    if( dp[i][j] != 0 ) return dp[i][j];
-    if( i == n || j == n ) return 0;
-
-    // Down
-    if( grid[i+1][j] != '*' && i+1 < n ) dp[i][j] += sol(i+1, j);
-
-    // Right
-    if( grid[i][j+1] != '*' && j+1 < n ) dp[i][j] += sol(i, j+1);
-
-    dp[i][j] %= Mod;
-
-    return dp[i][j];
-}
-
-
+/********** 2. Bottom Up Solution **********/
 bool flag = true;
 signed main(){
     opt;
@@ -46,6 +31,7 @@ signed main(){
                 flag = false;
                 break;
             }
+            
             if( grid[i][j] == '*' ) dp[i][j] = 0;
         }
     }
@@ -53,5 +39,62 @@ signed main(){
     // Set Known Value
     dp[n-1][n-1] = grid[n-1][n-1] == '*' ? 0 : 1;
 
-    if( flag ) cout << sol(0, 0);
+    for ( int i = n - 1 ; i >= 0 ; i-- ){
+        for( int j = n - 1 ; j >= 0 ; j-- ){
+            if( grid[i][j] == '*' ) continue;
+            
+            // Border
+            if( i + 1 < n ) dp[i][j] += dp[i+1][j];
+            if( j + 1 < n ) dp[i][j] += dp[i][j+1];
+
+            dp[i][j] %= Mod;
+        }
+    }
+
+    if( flag ) cout << dp[0][0];
 }
+/**********/
+
+/********** 1. Top Down Solution - TLE **********/
+// int sol( int i, int j ){
+//     if( dp[i][j] != 0 ) return dp[i][j];
+//     if( i == n || j == n ) return 0;
+
+//     // Down
+//     if( grid[i+1][j] != '*' && i+1 < n ) dp[i][j] += sol(i+1, j);
+
+//     // Right
+//     if( grid[i][j+1] != '*' && j+1 < n ) dp[i][j] += sol(i, j+1);
+
+//     dp[i][j] %= Mod;
+
+//     return dp[i][j];
+// }
+
+
+// bool flag = true;
+// signed main(){
+//     opt;
+
+//     // Input
+//     cin >> n;
+//     for( int i = 0 ; i < n ; i++ ){
+//         for( int j = 0 ; j < n ; j++ ){
+//             cin >> grid[i][j];
+
+//             if( i == 0 && j == 0 && grid[i][j] == '*' ){
+//                 cout << 0;
+//                 flag = false;
+//                 break;
+//             }
+            
+//             if( grid[i][j] == '*' ) dp[i][j] = 0;
+//         }
+//     }
+
+//     // Set Known Value
+//     dp[n-1][n-1] = grid[n-1][n-1] == '*' ? 0 : 1;
+
+//     if( flag ) cout << sol(0, 0);
+// }
+/**********/
