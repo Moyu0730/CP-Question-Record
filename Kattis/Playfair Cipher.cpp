9b6fc4f2723c155858ed4e528c4373250d48e489
Moyu0730 <aslanchen0730@gmail.com>
2023-05-13 01:15:00 +0800

    【2023. 05. 13】Playfair Cipher - Idk Why I Got RE , Haven't Solved

--- Kattis/Playfair Cipher.cpp
+++ Kattis/Playfair Cipher.cpp
@@ -12,9 +12,18 @@ using namespace std;
 
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
-string key, text, tmp;
+string key, text;
+vector<char> res;
 char grid[30][30];
 bool isUnique[30];
+vector<pair<int, int>> pos;
+
+struct node {
+    int i;
+    int j;
+    int ind;
+    char ch;
+};
 
 signed main(){
     // opt;
@@ -23,6 +32,8 @@ signed main(){
     getline(cin, text);
 
     for( int i = 0 ; i < 30 ; i++ ) isUnique[i] = true;
+    isUnique[16] = false;
+    pos.resize(30);
 
     // Generate Grid
     int ind = 0, prev = 0, rem = 25, i = 0, j = 0;
@@ -51,8 +62,10 @@ signed main(){
             }
 
             if( flag ){
-                grid[i][j] = ch;
+                grid[i][j] = char((int)ch-32);
                 isUnique[asc] = false;
+                pos[asc].first = i;
+                pos[asc].second = j;
                 rem--;
                 ind++;
             }else{
@@ -63,10 +76,12 @@ signed main(){
 
             if( ind == key.size() - 1 ) isEnd = true;
         }else{
-            for( int t = prev ; t < 26 ; t++ ){
+            for( int t = prev ; t <= 26 ; t++ ){
                 if( isUnique[t] == true ){
-                    grid[i][j] = char(t+97);
+                    grid[i][j] = char(t+97-32);
                     isUnique[t] = false;
+                    pos[t].first = i;
+                    pos[t].second = j;
                     rem--;
                     prev = t;
                     break;
@@ -77,4 +92,57 @@ signed main(){
         i = j == 4 ? i+1 : i;
         j = j == 4 ? 0 : j+1;        
     }
+
+    for( int i = 0 ; i < text.size() ; i++ ) if( text[i] != ' ' ) res.emplace_back(text[i]);
+
+    node f, s;
+    int index = 0;
+    while( index != res.size() ){
+        // Classification First Char and Second Char in Digraphs
+        f.ch = res[index]; f.ind = index;
+        if( index < res.size() ){
+            index++;
+            s.ch = res[index]; s.ind = index;
+        }else{
+            s.ch == ' ';
+        }
+        
+        if( f.ch == s.ch ){
+            res.insert(res.begin()+index, 'x');
+            s.ch = res[index]; 
+            s.ind = index;
+        }
+        
+        if( index >= res.size()-1 && res.size() % 2 ){
+            s.ch = 'x';
+            res.emplace_back(s.ch);
+            s.ind = res.size()-1;
+        }
+
+        f.i = pos[(int)f.ch-97].first; 
+        f.j = pos[(int)f.ch-97].second;
+
+        s.i = pos[(int)s.ch-97].first; 
+        s.j = pos[(int)s.ch-97].second;
+
+        if( f.i == s.i ){
+            f.j = f.j == 4 ? 0 : f.j + 1;
+            res[f.ind] = grid[f.i][f.j];
+            s.j = s.j == 4 ? 0 : s.j + 1;
+            res[s.ind] = grid[s.i][s.j];
+        }else if( f.j == s.j ){
+            f.i = f.i == 4 ? 0 : f.i + 1;
+            res[f.ind] = grid[f.i][f.j];
+            s.i = s.i == 4 ? 0 : s.i + 1;
+            res[s.ind] = grid[s.i][s.j];
+        }else{
+            res[f.ind] = grid[f.i][s.j];
+            res[s.ind] = grid[s.i][f.j];
+        }
+        
+        index++;
+    }
+
+    cout << "\n";
+    for( auto i : res ) cout << i;
 }
