57296164d3bc4e22099f686a7bef23dca1d180df
Moyu0730 <aslanchen0730@gmail.com>
2023-07-02 14:59:17 +0800

    【2023. 07. 02】P1629. Postman Delivering Letter - Solved

---
+++ Luogu/P1629. Postman Delivering Letter.cpp
@@ -0,0 +1,54 @@
+/* Question : Luogu P1629. Postman Delivering Letter */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mem(x) memset(x, 0x3F, sizeof(x));
+
+const int MAXN = 1e3 + 50;
+const int Mod = 1e9 + 7;
+int n, m, v, u, w, res;
+int dis[MAXN];
+vector<vector<pair<int, int> > > posG, negG;
+
+void dij( vector<vector<pair<int, int> > > &G, int root ){
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+    pq.push({0, root});
+    dis[root] = 0;
+    while( !pq.empty() ){
+        int node = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
+        if( d > dis[node] ) continue;
+        for( auto i : G[node] ){
+            int v = i.first;
+            int w = i.second;
+            if( d + w < dis[v] ){
+                dis[v] = d + w;
+                pq.push({dis[v], v});
+            }
+        }
+    }
+}
+
+signed main(){
+    opt;
+    cin >> n >> m;
+    posG.resize(n+5);
+    negG.resize(n+5);
+    for( int i = 0 ; i < m ; i++ ){
+        cin >> u >> v >> w;
+        posG[u].push_back({v, w});
+        negG[v].push_back({u, w});
+    }
+
+    mem(dis);
+    dij(posG, 1);
+    for( int i = 1 ; i <= n ; i++ ) res += dis[i];
+    mem(dis);
+    dij(negG, 1);
+    for( int i = 1 ; i <= n ; i++ ) res += dis[i];
+
+    cout << res << "\n";
+}
