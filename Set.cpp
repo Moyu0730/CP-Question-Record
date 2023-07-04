ffb564fbcdbbefe287562a0376104d54c9b6c942
Moyu0730 <aslanchen0730@gmail.com>
2023-07-04 20:12:02 +0800

    Add memset Function to Define

--- Set.cpp
+++ Set.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 #define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mem(x) memset(x, 0x3F, sizeof(x));
 
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
