c0707a504011ef61e6ca571385fb43d912a3f499
Moyu0730 <aslanchen0730@gmail.com>
2023-07-08 13:27:17 +0800

    Add Lots of New Define

--- Set.cpp
+++ Set.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 #define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define mem(x) memset(x, 0x3F, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define f first
+#define s second
 
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
 
