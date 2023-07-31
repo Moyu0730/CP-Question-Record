d6d4ea6b2b3d77ee44257b3e35f1ab95295b6bd6
Moyu0730 <aslanchen0730@gmail.com>
2023-08-01 00:01:26 +0800

    【2023. 07. 31】Beginner Contest 100 - D - Patisserie ABC - Solved

--- AtCoder/Beginner Contest 100 - D - Patisserie ABC.cpp
+++ AtCoder/Beginner Contest 100 - D - Patisserie ABC.cpp
@@ -15,30 +15,15 @@ const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 1e3 + 50;
 const int Mod = 1e9 + 7;
 int n, m, b, t, p, res, cake[MAXN][3+5];
-vector<vector<int>> ans;
 
-void Enumerate( int k, int cnt ){
-    if( k == 0 ){
-        res = max(res, abs(b) + abs(t) + abs(p));
-        return;
-    }
-    if( cnt >= n ) return;
+int Sum(){
+    int ans = 0, t[MAXN];
+    for( int i = 0 ; i < n ; i++ ) t[i] = cake[i][0] + cake[i][1] + cake[i][2];
 
-    Enumerate(k, cnt+1);
+    sort(t, t+n, greater<int>());
+    for( int i = 0 ; i < m ; i++ ) ans += t[i];
 
-    ans.push_back({cake[cnt][0], cake[cnt][1], cake[cnt][2]});
-    b += cake[cnt][0];
-    t += cake[cnt][1];
-    p += cake[cnt][2];
-
-    Enumerate(k-1, cnt+1);
-    
-    ans.pop_back();
-    b -= cake[cnt][0];
-    t -= cake[cnt][1];
-    p -= cake[cnt][2];
-
-    return;
+    return ans;
 }
 
 signed main(){
@@ -47,7 +32,19 @@ signed main(){
     for( int i = 0 ; i < n ; i++ ) cin >> cake[i][0] >> cake[i][1] >> cake[i][2];
 
     res = -1;
-    Enumerate(m, 0);
+    for( int i = 0 ; i < 2 ; i++ ){
+        for( int ind = 0 ; ind < n ; ind++ ) cake[ind][0] *= -1;
 
-    cout << res;
+        for( int j = 0 ; j < 2 ; j++ ){
+            for( int ind = 0 ; ind < n ; ind++ ) cake[ind][1] *= -1;
+            
+            for( int k = 0 ; k < 2 ; k++ ){
+                for( int ind = 0 ; ind < n ; ind++ ) cake[ind][2] *= -1;
+
+                res = max( res, Sum() );
+            }
+        }
+    }
+
+    cout << res << "\n";
 }
