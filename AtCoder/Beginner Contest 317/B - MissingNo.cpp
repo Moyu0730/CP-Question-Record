58192496986c4d7b811fa80a893167cd05ce864b
Moyu0730 <aslanchen0730@gmail.com>
2023-08-27 23:57:14 +0800

    【2023. 08. 27】B - MissingNo - Solved

---
+++ AtCoder/Beginner Contest 317/B - MissingNo.cpp
@@ -0,0 +1,34 @@
+/* Question : AtCoder Beginner Contest 317 - B - MissingNo */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pirq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+int n, cnt, arr[MAXN];
+
+signed main(){
+    opt;
+    cin >> n;
+    for( int i = 0 ; i < n ; i++ ) cin >> arr[i];
+
+    sort(arr, arr+n);
+    cnt = arr[0];
+    for( int i = 1 ; i <= n ; i++, cnt++ ){
+        if( arr[i] != cnt + 1 ){
+            cout << cnt + 1 << "\n";
+            break;
+        }
+    }
+}
