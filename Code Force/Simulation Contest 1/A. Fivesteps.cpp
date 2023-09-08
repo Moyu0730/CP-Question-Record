e9c17ecc48d4f5a28d637fb53fb13c63f6fad7d5
Moyu0730 <aslanchen0730@gmail.com>
2023-09-08 15:39:30 +0800

    【2023. 09. 07】A. Fivesteps - Solved

--- Code Force/Simulation Contest 1/A. Fivesteps.cpp
+++ Code Force/Simulation Contest 1/A. Fivesteps.cpp
@@ -16,24 +16,36 @@ using namespace std;
 const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 40 + 50;
 const int Mod = 1e9 + 7;
-int n, d, step, arr[MAXN];
+int n, x, res, arr[MAXN];
+map<int, int> mp;
+
+void f1( int cnt, int amt, int sum ){
+    if( cnt == amt + 1 ){
+        mp[sum]++;
+        return;
+    }
+
+    f1(cnt+1, amt, sum + arr[cnt]);
+    f1(cnt+1, amt, sum - arr[cnt]);
+}
+
+void f2( int cnt, int amt, int sum, int tar ){
+    if( cnt == amt + 1 ){
+        res += mp[tar-sum];
+        return;
+    }
+
+    f2(cnt+1, amt, sum+arr[cnt], tar);
+    f2(cnt+1, amt, sum-arr[cnt], tar);
+}
 
 signed main(){
     opt;
-    cin >> n >> d;
-    for( int i = 0 ; i < n ; i++ ) cin >> arr[i];
-    
-    for( int i = 0 ; i <= ( 1 << n ) ; i++ ){
-        int tmp = i, res = 0, ind = 0;
-        while( ind <= n ){
-            if( i & ( 1 << ind) ) res += ( arr[ind] * -1 );
-            else res += arr[ind];
-            
-            ind++;
-        }
+    cin >> n >> x;
+    for( int i = 1 ; i <= n ; i++ ) cin >> arr[i];
 
-        if( res == d ) step++;
-    }
+    f1(1, n/2, 0);
+    f2(n/2+1, n, 0, x);
 
-    cout << step << "\n";
+    cout << res << "\n";
 }
