08257fe3b7bd20db61dfd1ff46214c8d272dba23
Moyu0730 <aslanchen0730@gmail.com>
2023-09-08 16:03:09 +0800

    【2023. 09. 02】C. Blue Spring - Solved

---
+++ AtCoder/Beginner Contest 318/C. Blue Spring.cpp
@@ -0,0 +1,39 @@
+/* Question : AtCoder Beginner Contest 318 - C - Blue Spring */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pirq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 2e5 + 50;
+const int Mod = 1e9 + 7;
+int n, d, p, res, sum, tk[MAXN];
+
+signed main(){
+    opt;
+    cin >> n >> d >> p;
+    for( int i = 0 ; i < n ; i++ ) cin >> tk[i];
+
+    sort(tk, tk+n, greater<int>());
+    
+    res = 0;
+    for( int i = 0 ; i < n ; i += d ){
+        sum = 0;
+        for( int j = 0 ; j < d ; j++ ){
+            if( i + j == n ) break;
+            sum += tk[i+j];
+        }
+        res += min(sum, p);
+    }
+    
+    cout << res << "\n";
+}
