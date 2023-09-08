87b1884c49de15c03a6ffa0bccfd4248597a7d1e
Moyu0730 <aslanchen0730@gmail.com>
2023-09-08 15:52:44 +0800

    【2023. 09. 02】A. Tcdr - Solved

---
+++ AtCoder/Beginner Contest 315/A. Tcdr.cpp
@@ -0,0 +1,30 @@
+/* Question : AtCoder Beginner Contest 315 - A - Tcdr */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pirq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+string str, res;
+
+signed main(){
+    opt;
+    cin >> str;
+    for( int i = 0 ; i < str.size() ; i++ ){
+        if( str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ) continue;
+        res.pb(str[i]);
+    }
+
+    cout << res << "\n";
+}
