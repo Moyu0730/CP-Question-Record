bd16e709fd96af1165b6affd50744afc150594e0
Moyu0730 <aslanchen0730@gmail.com>
2023-10-24 23:32:56 +0800

    【2023. 10. 18】D. Knapsack 1 - Solved

---
+++ AtCoder/Educational DP Contest/D. Knapsack 1.cpp
@@ -0,0 +1,31 @@
+/* Question : OJ Number */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define priq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e2 + 50;
+const int Mod = 1e9 + 7;
+int n, w, weight[MAXN], val[MAXN], dp[100050];
+
+signed main(){
+    opt;
+    cin >> n >> w;
+    for( int i = 0 ; i < n ; i++ ) cin >> weight[i] >> val[i];
+
+    for( int i = 0 ; i < n ; i++ ){
+        for( int j = w ; j >= weight[i] ; j-- ) dp[j] = max( dp[j], dp[j-weight[i]] + val[i] );
+    }
+
+    cout << dp[w] << "\n";
+}
