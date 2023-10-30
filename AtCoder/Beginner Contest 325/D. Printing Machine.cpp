68b6571c3850d3d4357de8a6a368c22a6586c2d7
Moyu0730 <aslanchen0730@gmail.com>
2023-10-31 00:23:54 +0800

    【2023. 10. 30】D. Printing Machine - Solved

---
+++ AtCoder/Beginner Contest 325/D. Printing Machine.cpp
@@ -0,0 +1,54 @@
+/* Question : AtCoder Beginner Contest 325 - D. Printing Machine */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define priq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+int n, t, d, res;
+multiset<pii> cnt;
+multiset<int> st;
+
+signed main(){
+    opt;
+    cin >> n;
+    for( int i = 0 ; i < n ; i++ ){
+        cin >> t >> d;
+        cnt.insert({t, t+d});
+    }
+    
+    pii tmp;
+    for( int now = 0 ;; now++ ){
+        if( st.empty() ){
+            if( cnt.empty() ) break;
+            tmp = *cnt.begin();
+            now = tmp.f;
+        }
+
+        while( !cnt.empty() && tmp.f == now ){
+            st.insert(tmp.s);
+            cnt.erase(cnt.begin());
+            tmp = *cnt.begin();
+        }
+
+        while ( !st.empty() && *st.begin() < now ) st.erase(st.begin());
+
+        if( !st.empty() ){
+            res++;
+            st.erase(st.begin());
+        }
+    }
+
+    cout << res << "\n";
+}
