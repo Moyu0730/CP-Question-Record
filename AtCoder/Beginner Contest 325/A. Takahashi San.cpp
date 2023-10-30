d360822dbe34a9fbe55bab0f6164179cff53e05b
Moyu0730 <aslanchen0730@gmail.com>
2023-10-31 00:23:11 +0800

    【2023. 10. 30】A. Takahashi San - Solved

---
+++ AtCoder/Beginner Contest 325/A. Takahashi San.cpp
@@ -0,0 +1,26 @@
+/* Question : AtCoder Beginner Contest 325 - A. Takahashi San */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define priq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+string str;
+
+signed main(){
+    cin >> str;
+    cout << str << " ";
+    cin >> str;
+    cout << "san\n";
+}
