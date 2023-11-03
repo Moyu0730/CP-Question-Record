438d325ca75193763e6dba5c1e3092cde8fd5e66
Moyu0730 <aslanchen0730@gmail.com>
2023-11-03 23:03:06 +0800

    【2023. 11. 03】1158. Book Shop - Solved

---
+++ CSES/1158. Book Shop.cpp
@@ -0,0 +1,35 @@
+/* Question : CSES 1158. Book Shop */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define priq(type) priority_queue<type, vector<type>, greater<type>>
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define f first
+#define s second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e5 + 50;
+const int Mod = 1e9 + 7;
+int n, x, dp[MAXN], cost[MAXN], page[MAXN];
+
+signed main(){
+    opt;
+    cin >> n >> x;
+
+    for( int i = 0 ; i < n ; i++ ) cin >> cost[i];
+    for( int i = 0 ; i < n ; i++ ) cin >> page[i];
+
+    for( int i = 0 ; i < n ; i++ ){
+        for( int j = x ; j >= cost[i] ; j-- ){
+            dp[j] = max(dp[j], dp[j-cost[i]] + page[i]);
+        }
+    }
+
+    cout << dp[x] << "\n";
+}
