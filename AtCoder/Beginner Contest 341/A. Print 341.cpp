8947c3244f6b0beec6cb68d6d4f9b5d0d625ea4e
Moyu0730 <aslanchen0730@gmail.com>
2024-02-19 22:07:15 +0800

    【2024. 02. 19】A. Print 341 - Solved

---
+++ AtCoder/Beginner Contest 341/A. Print 341.cpp
@@ -0,0 +1,30 @@
+/* Question : AtCoder Beginner Contest 341 - A. Print 341 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define F first
+#define S second
+#define X first
+#define Y second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+const int INF = 1e9;
+int n;
+
+signed main(){
+    opt;
+    cin >> n;
+
+    cout << "1";
+    for( int i = 0 ; i < n ; i++ ) cout << "01";
+    cout << "\n";
+}
