71d97f2bcb570cc77939951004e3f9c750b2516b
Moyu0730 <aslanchen0730@gmail.com>
2024-07-02 16:05:36 +0800

    【2024. 07. 02】2165. Tower of Hanoi - Solved

--- CSES/2165. Tower of Hanoi.cpp
+++ CSES/2165. Tower of Hanoi.cpp
@@ -16,9 +16,28 @@ using namespace std;
 const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
-int n;
+int n, cnt, start[MAXN], target[MAXN];
+
+void solve( int n, int from, int to, int sup ){
+    if( n == 1 ){
+        start[cnt] = from;
+        target[cnt] = to;
+        cnt++;
+
+        return;
+    }
+
+    solve(n-1, from, sup, to);
+    solve(1, from, to, sup);
+    solve(n-1, sup, to, from);
+}
 
 signed main(){
     opt;
     cin >> n;
+
+    solve(n, 1, 3, 2);
+
+    cout << cnt << "\n";
+    for( int i = 0 ; i < cnt ; i++ ) cout << start[i] << " " << target[i] << "\n";
 }
