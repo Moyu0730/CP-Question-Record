c15320d42e1f84daac929a740187ec76f270c6ab
Moyu0730 <aslanchen0730@gmail.com>
2024-09-14 13:09:39 +0800

    【2024. 09. 14】Opt Setup Code

---
+++ Setup.cpp
@@ -0,0 +1,26 @@
+/* Question : OJ Number */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define mem(x, value) memset(x, value, sizeof(x));
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define pb push_back
+#define F first
+#define S second
+#define X first
+#define Y second
+#define int long long
+
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+const int MAXN = 1e8 + 50;
+const int Mod = 1e9 + 7;
+const int INF = 1e9;
+
+signed main(){
+    #ifndef LOCAL
+        opt;
+    #endif
+}
