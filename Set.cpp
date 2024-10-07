d083e09a1557e324324abf776619f945abf712c3
Moyu0730 <aslanchen0730@gmail.com>
2024-10-07 16:37:43 +0800

    【2024. 10. 07】Update Set.cpp - Solved

--- Set.cpp
+++ Set.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 #define opt ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pirq(type) priority_queue<type, vector<type>, greater<type>>
 #define mem(x, value) memset(x, value, sizeof(x));
 #define pii pair<int, int>
 #define pdd pair<double, double>
@@ -13,12 +14,13 @@ using namespace std;
 #define X first
 #define Y second
 #define int long long
+#define ll long long
 
 const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 1e8 + 50;
 const int Mod = 1e9 + 7;
-const int LLINF = 1e18;
-const int INF = 1e9;
+const long long LLINF = 0x7FFFFFFFFFFFFFFF;
+const int INF = 0x7FFFFFFF;
 
 signed main(){
     opt;
