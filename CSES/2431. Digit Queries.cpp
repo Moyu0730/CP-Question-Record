a733903ef628576009db4fa9e2d0e821b9c5688a
Moyu0730 <aslanchen0730@gmail.com>
2024-11-19 18:19:36 +0800

    【2024. 11. 19】2431. Digit Queries - Solved

--- CSES/2431. Digit Queries.cpp
+++ CSES/2431. Digit Queries.cpp
@@ -16,17 +16,32 @@ using namespace std;
 const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 const int MAXN = 1e3 + 50;
 const int Mod = 1e9 + 7;
-int n, arr[MAXN];
+int q, cnt;
 
 signed main(){
     opt;
-    cin >> n;
-    for( int i = 0 ; i < n ; i++ ) cin >> arr[i];
-    for( int i = 0 ; i < n ; i++ ){
-        if( i <= 9 ){
-            cout << i;
-        }else if(){
-            
+    cin >> q;
+
+    while( q-- ){
+        cin >> cnt;
+
+        int count = 1, tmp = cnt, amt = 9, acc = 0;
+        while( tmp > amt * count ){
+            tmp -= amt * count;
+            count++;
+            amt *= 10;
+            acc += amt;
         }
+
+        int value = ( acc / 10 ) + ( (tmp+count-1) / count );
+        for(int i = 0 ; i < count - ( (tmp+count-1) % count ) - 1 ; i++ ) value /= 10;
+
+        cout << value % 10 << "\n"; 
     }
 }
+
+/*
+1 ~ 9      (9)        -> 1
+10 ~ 99    (90 * 2)   -> 2
+100 ~ 999  (900 * 3)  -> 3
+*/
