ed61a7566d6cd786aace42c61396caaa32351812
Moyu0730 <aslanchen0730@gmail.com>
2025-08-07 12:35:40 +0800

    【2025. 08. 06】CSES 1732 - Solved

---
+++ CSES/1732. Finding Borders.cpp
@@ -0,0 +1,89 @@
+/* Question : CSES 1732. Finding Borders */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Pragma */
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+/* Self Define */
+#define IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define MEM(_array, _value) memset(_array, _value, sizeof(_array));
+#define ALL(_array) _array.begin(), _array.end()
+#define LB(_array, v) lower_bound(ALL(_array), v)
+#define UB(_array, v) upper_bound(ALL(_array), v)
+#define vc(_data) vector<_data>
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define mkp make_pair
+#define sz size()
+#define pb push_back
+#define F first
+#define S second
+#define int long long
+#define ll long long
+#define ld long double
+#define tpn typename
+
+/* Self Define Template Function */
+template <typename T> void pV(vector<T> _vector ) {
+    for( auto _it : _vector ) cout << _it << " ";
+    cout << "\n";
+}
+
+template <typename A, typename B> bool boundry( pair<A, B> &_pair, int _n, int _m ){
+    return 1 <= _pair.F && _pair.F <= _n && 1 <= _pair.S && _pair.S <= _m;
+}
+
+template <typename A, typename B> istream& operator>>( istream& _is, pair<A, B> &_pair ){ 
+    return _is >> _pair.F >> _pair.S;
+}
+
+template <typename A, typename B> ostream& operator<<( ostream& _os, pair<A, B> _pair ){ 
+    return _os << '(' << _pair.F << " " << _pair.S << ')';
+}
+
+/* Self Define Const */
+const auto dir = vector< pair<int, int> > { {1, 0}, {0, 1},  {-1, 0}, {0, -1}, 
+                                            {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+const int         MAXN = 1e7 + 50;
+const int          Mod = 1e9 + 7;
+const int          INF = 0x7FFFFFFF;
+const ll         LLINF = 0x7FFFFFFFFFFFFFFF;
+const int       MEMINF = 0x3F;
+const int   MEMINF_VAL = 0x3F3F3F3F;
+const ll  MEMLLINF_VAL = 0x3F3F3F3F3F3F3F3F;
+
+int hsh[MAXN], ksm[MAXN];
+string str;
+int p = 100;
+
+int query( int l, int r ){
+    if( l == 0 ) return hsh[r];
+    else return ( ( hsh[r] - ( hsh[l - 1] * ksm[r - l + 1] ) ) % Mod + Mod ) % Mod;
+}
+
+inline void solve(){
+    cin >> str;
+
+    for( int i = 0 ; i < str.sz ; ++i ){
+        hsh[i] = i == 0 ? str[i] : ( ( hsh[i-1] * p ) % Mod + str[i] ) % Mod;
+        ksm[i] = i == 0 ? 1 : ( ksm[i-1] * p ) % Mod;
+    }
+
+    for( int i = 1 ; i < str.sz ; ++i ){
+        if( query(0, i-1) == query(str.sz - 1 - i + 1, str.sz - 1) ) cout << i << " ";
+    }
+}
+
+signed main(){
+    IO;
+
+    int T = 1;
+    // cin >> T;
+    while( T-- ){
+        solve();
+    }
+}
